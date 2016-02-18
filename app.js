8b1fb8a7fba66a84b44d5bc49a699b614683b5fa
Vengg <alexandercallan@gmail.com>
2016-02-18 17:50:38 -0500

    Changed Error

--- app.js
+++ app.js
@@ -83,16 +83,16 @@ app.post('/api', function(req, res) {
 			}
 			else {
 				console.log("This is the Body");
-				//console.log(body);
+				console.log(body);
 				console.log("This is the Response");
-				//console.log(response);
+				console.log(response);
 				console.log("This is what is being sent back");
-				//console.log(res);
-				response.pipe(res);
+				
 			}
 		})
 	);
-	
+	//console.log(res);
+	fwdurl.pipe(res);
 	
 	
 });
